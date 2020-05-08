7fc09aa00bdeb4dd22152d7072a3ae81934a1586
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-08 20:25:37 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,18 +1,19 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1> 8==D </h1>
     `;
 
-    customElements.define('com-sap-sample-big-cock', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-sap-sample-big-cock', class template extends HTMLElement {
 
 
 		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
-		}
+    super(); 
+    this._shadowRoot = this.attachShadow({mode: "open"});
+    this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    this._tagContainer;
+    this._tagType = "h1";
+    this._tagText = "Hello World";
+}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
@@ -54,8 +55,20 @@
         }
         */
 
-        redraw(){
+       redraw(){
+    if (this._tagText != null){
+        if (this._tagContainer){
+            this._tagContainer.parentNode.removeChild(this._tagContainer);
         }
+
+        var shadow = window.getSelection(this._shadowRoot);
+        this._tagContainer = document.createElement(this._tagType);
+        var theText = document.createTextNode(this._tagText);    
+        this._tagContainer.appendChild(theText); 
+        this._shadowRoot.appendChild(this._tagContainer);
+    }
+}
+	    
     });
 })();
 
