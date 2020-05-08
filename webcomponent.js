249c54a587784efd283ad30b5334658b66b2447b
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-08 19:02:45 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,7 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Twoja Stara</h1>
+        <h1> 8==D </h1>
     `;
 
     customElements.define('com-sap-sample-helloworld9', class HelloWorld1 extends HTMLElement {
